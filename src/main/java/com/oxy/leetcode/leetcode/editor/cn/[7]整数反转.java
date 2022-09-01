d41a1560a9867b446d29b348486baaedc95dc58c
ouxuyong <18289422015@163.com>
2022-09-01 22:57:28 +0800

    提交解题代码

---
+++ src/main/java/com/oxy/leetcode/leetcode/editor/cn/[7]整数反转.java
@@ -0,0 +1,72 @@
+//给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。 
+//
+// 如果反转后整数超过 32 位的有符号整数的范围 [−231, 231 − 1] ，就返回 0。 
+//假设环境不允许存储 64 位整数（有符号或无符号）。
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：x = 123
+//输出：321
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：x = -123
+//输出：-321
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：x = 120
+//输出：21
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：x = 0
+//输出：0
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// -231 <= x <= 231 - 1 
+// 
+// Related Topics 数学 
+// 👍 3613 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class ReverseSolution {
+    public static void main(String[] args) {
+        int num = reverse(1232);
+        System.out.println(num);
+    }
+    public static int reverse(int x) {
+        String str = Integer.toString(x);
+        char[] chars = str.toCharArray();
+        StringBuffer stringBuffer = new StringBuffer();
+        for(int i = chars.length -1; i >= 0; i-- ){
+            if(chars[i] == '-' || chars[i] == '+'){
+                stringBuffer = new StringBuffer(chars[i] + stringBuffer.toString());
+                continue;
+            }
+            stringBuffer.append(chars[i]);
+        }
+        Long aLong = Long.parseLong(stringBuffer.toString());
+        if(aLong > Integer.MAX_VALUE || aLong < Integer.MIN_VALUE){
+            return 0;
+        }
+
+        return aLong.intValue();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
